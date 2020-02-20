da31e6381d9cfc5b9068a85b4af326171f2621ae
Kahvipuu <partaar@hotmail.com>
2020-02-20 16:35:33 +0200

    default date wtr formiin

--- application/worktimerecords/forms.py
+++ application/worktimerecords/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, validators, IntegerField, DateField, SelectField
+from datetime import date
 
 class WorktimerecordForm(FlaskForm):
     name = StringField("Type of work done", [validators.Length(min=3, message="min 3 characters")])
     done = BooleanField("Done")
     hours = IntegerField("Hours", [validators.number_range(min=0, max=24, message="Hours 0-24")])
-    dateofwork = DateField("Date(YYYY-MM-DD)", [validators.input_required(message="YYYY-MM-DD")])
+    dateofwork = DateField("Date(YYYY-MM-DD)", [validators.input_required(message="YYYY-MM-DD")], default=date.today())
     project = SelectField("Project Name", coerce=int)
 
     class Meta:
